7437af0283dbeb143d4f8b855bcd6565b3978747
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2020-03-11 23:12:24 +0300

    10 comit

--- src/js/modules/consts.js
+++ src/js/modules/consts.js
@@ -1,11 +1,9 @@
 const NOW = new Date();
 
-// NOW_DATE - конвертируем дату в UTC формат, для корректного отображения даты в карточке
 export const NOW_DATE = new Date(NOW.getTime() + NOW.getTimezoneOffset() * 60000);
 
 export const WEEK_AGO_DATE = new Date(NOW_DATE.getFullYear(), NOW_DATE.getMonth(), NOW_DATE.getDate());
 
-// устанавливаем дату - неделя назад
 WEEK_AGO_DATE.setDate(WEEK_AGO_DATE.getDate() - 6);
 
 export const MONTHS_FOR_CONVERT_DATE = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
@@ -23,5 +21,5 @@ export const COUNTRY_NEWS = 'ru';
 export const XSS_WARNING = ['<', '>', '(', ')', ';', '=', '/', '.'];
 export const SPACES_DELETE_REG = /\s+/g;
 
-export const GIT_HUB_URL = 'https://api.github.com/repos/IvanTomashevskiy/diplom/commits';
+export const GIT_HUB_URL = 'https://api.github.com/repos/yurj9999/diplom/commits';
 export const CARDS_IN_LINE = 3;
